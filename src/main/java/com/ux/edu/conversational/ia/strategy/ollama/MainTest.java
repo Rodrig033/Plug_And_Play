8588bf3dcfa1118c7c77dbc24994ab60d4b1dc7d
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2026-04-30 07:19:28 -0600

    Implementación Ollama - Java

--- src/main/java/com/ux/edu/conversational/ia/strategy/ollama/MainTest.java
+++ src/main/java/com/ux/edu/conversational/ia/strategy/ollama/MainTest.java
@@ -2,10 +2,13 @@ package com.ux.edu.conversational.ia.strategy.ollama;
 
 public class MainTest {
     public static void main(String[] args) {
+        // Prompt (input)
         OllamaRespuesta respuesta = OllamaCliente.generar("Explica qué es POO");
 
         if (respuesta != null) {
+            // Agente
             System.out.println("Modelo: " + respuesta.getModel());
+            // Respuesta (output)
             System.out.println("Respuesta: " + respuesta.getResponse());
         }
 
